2f8b0ee4d8de60aa035a490a9e54c43125046b3c
mjcleme <mjcleme@gmail.com>
2016-03-28 22:27:51 +0000

    foobar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,24 @@
-angular.module('chatApp', [])
-.controller('chatController', ["$scope","$firebaseArray", 
-  function($scope,$firebaseArray) {
-      var ref = new Firebase("https://incandescent-torch-9913.firebaseio.com/");
-	  $scope.chats = $firebaseArray(ref);
-
-	  $scope.update = function(user) {
-		var tmp = {name:user.name, chat:user.chat};
-		$scope.chats.$add(tmp);
-		user.chat = "";
-	  };
-
-}]);
+var myApp = angular.module("myApp", ["firebase"]);
+myApp.controller("MyController", ["$scope", "$firebaseArray",
+function($scope, $firebaseArray) {
+  //CREATE A FIREBASE REFERENCE
+  var ref = new Firebase("https://incandescent-torch-9913.firebaseio.com/");
+  // GET MESSAGES AS AN ARRAY
+  $scope.messages = $firebaseArray(ref);
+  //ADD MESSAGE METHOD
+  $scope.addMessage = function(e) {
+	//LISTEN FOR RETURN KEY
+	if (e.keyCode === 13 && $scope.msg) {
+	  //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+	  var name = $scope.name || "anonymous";
+	  //ADD TO FIREBASE
+	  $scope.messages.$add({
+		from: name,
+		body: $scope.msg
+	  });
+	  //RESET MESSAGE
+	  $scope.msg = "";
+	}
+  }
+}
+]);
